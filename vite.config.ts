f0deb4fdb07661c3728517c5f44e3c39c5dfd050
Trowsing <trowsing@gmail.com>
2026-02-27 21:54:02 -0300

    feat: change base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/json-parser/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
